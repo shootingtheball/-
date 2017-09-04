bc88ca2d1ec764f0ced360ffc92043a730e84748
张廷玉 <412753988@qq.com>
2017-09-04 23:02:15 +0800

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app">
+		<v-header :seller="seller"></v-header>
+	</div>
+</template>
+
+<script>
+
+	import vHeader from './components/head/v-header';
+
+	export default {
+		name: 'app',
+		data() {
+			return{
+				seller:""
+			}
+		},
+		components: {
+			vHeader
+		},
+		created() {
+			this.$http.get('api/seller').then((response) => {
+				response=response.body;
+				if (response.errno ===0) {
+					this.seller = response.data;
+				}
+			})
+		}
+	}
+</script>
+
+<style>
+	*{
+		margin:0;
+		padding: 0;
+	}
+</style>
