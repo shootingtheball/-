c930f8728429c43ab9527eb38e09e04115e63325
张廷玉 <412753988@qq.com>
2017-09-20 18:38:47 +0800

    Create v-star.vue

---
+++ v-star.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="vStar" :class="detialStar">
+    <template>
+      <span v-for="star in stars" class="vStars"  :class="detialStars">
+      <img :src="star.picStar">
+    </span>
+    </template>
+  </div>
+</template>
+
+<script>
+var picStar = require("./star.png");
+var picStar2 = require("./star2.png")
+
+export default {
+  name:"vStar",
+  data (){
+    return {
+      stars:[]
+    }
+  },
+  props:{
+    size:{
+      type : Number
+    },
+    score:{
+      type : Number
+    }
+  },
+  computed:{
+    detialStar() {
+      var compare =this.score-(Math.floor(this.score)+0.5);
+      var integer = Math.floor(this.score);
+
+      for (var i = 0; i< integer; i++) {
+        this.stars.push({stars:picStar}) 
+      }
+      
+      if (compare>0) {
+        this.stars.push({stars:picStar2})
+      }
+    }
+  }
+}
+</script>
