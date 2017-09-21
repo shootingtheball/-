b31dc95796a5e399e407b1aad31c1c5c8f512c8c
张廷玉 <412753988@qq.com>
2017-09-21 18:55:09 +0800

    Update v-star.vue

--- v-star.vue
+++ v-star.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="vStar" :class="detialStar">
     <template>
-      <span v-for="star in stars" class="vStars"  :class="detialStars">
-      <img :src="star.picStar">
+      <span v-for="star in stars" class="vStars" >
+      <img :src="star.picture">
     </span>
     </template>
   </div>
@@ -31,15 +31,26 @@ export default {
     detialStar() {
       var compare =this.score-(Math.floor(this.score)+0.5);
       var integer = Math.floor(this.score);
-
+      this.stars=this.stars.splice(0,this.stars.length);
+      console.log(this.stars.length)
       for (var i = 0; i< integer; i++) {
-        this.stars.push({stars:picStar}) 
+        this.stars.push({picture:picStar}) 
       }
       
-      if (compare>0) {
-        this.stars.push({stars:picStar2})
+      if (compare>=0) {
+        this.stars.push({picture:picStar2})
       }
     }
   }
 }
 </script>
+
+<style>
+.vStar{
+  border: 1px solid yellow
+}
+.vStars img{
+  width: 100px;
+}
+</style>
+
