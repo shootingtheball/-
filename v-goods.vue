ba82003b62524956d93831bdc4478aa3c31f6299
张廷玉 <412753988@qq.com>
2017-09-25 19:25:00 +0800

    1
    1

---
+++ v-goods.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="goods">
+      <div class="goods-menu">
+          <ul>
+              <li></li>
+          </ul>
+      </div>
+      <div class="goods-food"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"vGoods",
+  data() {
+      return{
+          goods:""
+      }
+  },
+ /* created() {
+      this.$http.get("api/goods").then((response)=>{
+          response = response.body
+          if(response.errno===0){
+              this.goods= response.data
+          }
+      })
+  } */
+}
+</script>
+
+<style>
+.goods{
+    display: flex;
+    position: absolute;
+    width: 100%;
+    top:400px;
+    bottom: 100px;
+    z-index: -1;
+}
+.goods-menu{
+    flex: 0 0 120px;
+    width: 120px;
+    background: #EDEDED;
+
+}
+.goods-food{
+    flex: 1
+}
+</style>
+
