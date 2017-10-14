e5fe81e31bfb6b2c672f42b5cfc76c35f8da3b62
张廷玉 <412753988@qq.com>
2017-10-14 18:16:04 +0800

    Update cartcontrol.vue

--- cartcontrol.vue
+++ cartcontrol.vue
@@ -2,9 +2,7 @@
   <div class="cartcontrol">
     <transition name="move">
     <div class="cart-decrease "  v-show="food.count>0" @click="decreaseCart">
-        <transition name="move2" v-show="food.count>0">
-        <span v-show="food.count>0" class="fa fa-minus-circle"></span>
-        </transition>
+        <span  class="inner fa fa-minus-circle"></span>
     </div>
     </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
@@ -14,7 +12,6 @@
 
 <script>
 import Vue from "vue";
-
 export default {
   props:{
       food:{
@@ -29,9 +26,9 @@ export default {
           if(!this.food.count){
               Vue.set(this.food,"count",1)
           }else{
-              this.food.count +=1
+              this.food.count +=1;
           }
-         console.log(1)
+          this.$emit("increment")
       },
       decreaseCart() {
           if(!event._constructed){
@@ -50,7 +47,6 @@ export default {
     font-size: 0;
     
 }
-
 .cart-count{
     display:inline-block;
     vertical-align: top;
@@ -60,39 +56,31 @@ export default {
     text-align: center;
     color: #93999f;
     font-size:10px;
+    transform: translate3d(0, 0, 0)
 }
 .cart-add,.cart-decrease{
     display:inline-block;
     padding: 6px;
-    font-size: 24px;
+    font-size: 30px;
     line-height: 24px;
     width: 24px;
     height: 24px;
     color:#00a0dc;
+    z-index: 1;
 }
 .move-enter-active{
-    transition: all 0.4s linear
+    transition: all 0.4s linear;
 }
 .move-leave-active{
-    transition: all 0.4s linear
+    transition: all 0.4s linear;
 }
-.move-enter,.move-leave-to{
+.move-enter,.move-leave-active{
     opacity: 0;
-    transform: translate(20px)
-}
-.move2-enter,.move2-leave-to{
-    color: red;
-    transform: rotate(180deg)
-}
-.move2-enter-active{
-    transform: all 0.4s linear;
-}
-.move2-leave-active{
-    transform: all 0.4s linear;
+    transform: translate3d(24px, 0, 0)
 }
 .cart-decrease span{
     display: inline-block;
+    transform: rotate(0deg);
+    transition: all 0.4s linear;
 }
 </style>
-
-
